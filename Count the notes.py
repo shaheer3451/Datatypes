515ffefcf13c04ad12639914dd8465930ad7e82e
Shaheer <sobiariaz1985@gmail.com>
2024-11-07 21:57:00 +0500

    Count the notes

--- Count the notes.py
+++ Count the notes.py
@@ -6,4 +6,4 @@ note_2 = (Amount%100)//50
 note_3 = (Amount%100)(50)//10
 print("notes of 100 rupee" , note_1)
 print("notes of 50 rupee" , note_2)
-print("notes of 10 rupee , note_3")
+print("notes of 10 rupee" , note_3)
