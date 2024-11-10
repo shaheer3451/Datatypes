70efc1a30a72b4594115ab1d42167b7648e8766c
Shaheer <sobiariaz1985@gmail.com>
2024-11-10 13:48:56 +0500

    profitloss

---
+++ conditional/profitloss.py
@@ -0,0 +1,9 @@
+actual_cost = int(input("give me the actual cost"))
+sell_cost = int(input("give me the actual sell cost"))
+
+
+if (sell_cost > actual_cost):
+   print("this is profit")
+   
+else:
+    print("this is loss")
