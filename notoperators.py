96469c0c7a74da4fde7685cbaeb25e321fc6f107
Shaheer <sobiariaz1985@gmail.com>
2024-11-17 21:06:14 +0500

    notoperators

---
+++ notoperators.py
@@ -0,0 +1,17 @@
+a =12
+d = 17 
+w = 12
+if a != d:
+    print(True)
+else:
+    print(False)
+if w == a:
+    print (True)
+else:
+    print(False)
+           
+
+a = "bottel"
+b = "glass"
+if a != b:
+    print(a,  'and', b, 'are different' )
