2f56bb18ba9394655bf6ef49d92341b267b50c79
Shaheer <sobiariaz1985@gmail.com>
2024-11-30 13:20:48 +0500

    divisiblenumbers

---
+++ Pythonchallengers/divisiblenumbers.py
@@ -0,0 +1,10 @@
+print("Enter your numerator")
+a = int(input())
+print("Enter your denominator")
+b = int(input())
+
+if a%b == 0 :
+    print(str(a) + " is divisible by " + str(b))
+else:
+     print(str(a) + " is not divisible by " + str(b))
+    
